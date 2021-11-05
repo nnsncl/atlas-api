d133b08e638d425c9b557ddd371636d8be62d4f3
nnsncl <nuniroland@gmail.com>
2021-11-05 20:08:38 +0100

    Error details with fastAPI features

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
@@ -28,6 +28,12 @@ stored_posts = [
 ]
 
 
+def find_post(id):
+    for post in stored_posts:
+        if post['id'] == id:
+            return post
+
+
 @app.get("/")
 def root():
     return {"message": "Atlas API"}
@@ -42,20 +48,21 @@ def get_posts():
 def create_post(post: Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0, 10e9)
-
     stored_posts.append(post_dict)
     return {"data": post_dict}
 
 
-# In "/posts/{id}", {id} is the Path Parameter
 @app.get("/posts/{id}")
-def get_post(id: int):
-
-    def find_post(id):
-        for post in stored_posts:
-            if post['id'] == id:
-                return post
-
+def get_post(id: int, response: Response):
     filtered_post = find_post(id)
+    if not filtered_post:
+        response.status_code = status.HTTP_404_NOT_FOUND
 
     return {"data": filtered_post}
+
+
+@app.delete('posts/{id}')
+def delete_post(id: int):
+    post_to_delete = find_post(id)
+    return {f"Post {post_to_delete} has been deleted."}
+
