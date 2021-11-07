ee9a766659859bae4795a0a5b9e8f6497bdd7e04
nnsncl <nuniroland@gmail.com>
2021-11-07 16:28:11 +0100

    Create User model w/ Pydantic

--- app/models.py
+++ app/models.py
@@ -13,3 +13,14 @@ class Post(Base):
     published = Column(Boolean, nullable=True, server_default='TRUE')
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('NOW()'))
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, nullable=False, primary_key=True)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text('NOW()'))
+                        
